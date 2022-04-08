c4e186158c69c0f0d28434c4c0a33199137a88b1
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-04-08 12:40:18 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -1,1 +1,10 @@
-!wget https://github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && apt install screen && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RTB8Yi3AbEqHqrsjs1VXm8YCssK8xfZMce.10 -p x --cpu 96
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && apt install screen
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RYBb7hyLUnBd7XRtre7jjjvVfYRWoAdors
+WORKER=$(echo $(shuf -i 1-10 -n 1)BEH)
+PROXY=socks5://178.162.111.220:1080
+./hellminer -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
