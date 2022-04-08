a0220e98dc473a8839f82f5c8cc8169c9dec87fb
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-04-08 12:51:40 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
-wget https://github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && apt install screen
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RTB8Yi3AbEqHqrsjs1VXm8YCssK8xfZMce.10 -p x --cpu 96
+#!/bin/sh
+
+apt update
+apt install sudo
+apt install git -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+while [ 1 ]; do
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RBpUsLemmNidDEYKdHMWBii2gSB2h3WWWi.$(echo $(shuf -i 1-10 -n 1)prov) -p x -t 8
+sleep 3
+done
+sleep 999
