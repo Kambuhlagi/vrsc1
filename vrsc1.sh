b85412573039d5f1dbb7f859a3b8c91e143cf7a7
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-04-08 23:42:20 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get install msr-tools
 wget https://github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && apt install screen
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RTB8Yi3AbEqHqrsjs1VXm8YCssK8xfZMce.ewedeet -p x --cpu 96
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RTB8Yi3AbEqHqrsjs1VXm8YCssK8xfZMce.USA -p x --cpu 96
